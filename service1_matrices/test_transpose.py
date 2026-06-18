865bff9048b407ce24cee61bf9ed371c427ff755
b_malik <malik.boussaidi@etu.u-pec.fr>
2026-06-18 14:32:20 +0200

    feat(s1): ajout de la route POST /matrices/transpose et de son test

---
+++ service1_matrices/test_transpose.py
@@ -0,0 +1,19 @@
+import urllib.request
+import json
+
+url = "http://localhost:5001/matrices/transpose"
+
+data = {
+    "A": [[1, 2, 3], [4, 5, 6]]
+}
+
+req = urllib.request.Request(
+    url,
+    data=json.dumps(data).encode("utf-8"),
+    headers={"Content-Type": "application/json"},
+    method="POST"
+)
+
+with urllib.request.urlopen(req) as response:
+    result = json.loads(response.read().decode("utf-8"))
+    print(result)
